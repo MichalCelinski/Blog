e049b1286e0c1a0b0a356893a3cc75553f94c475
Michal Celinski <michal.celinski@gmail.com>
2017-09-15 11:41:35 +0200

    bootstrap usage and  pagination for list view.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Article
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class ArticlesListView(View):
 
     def get(self, request):
         articles_list = Article.objects.filter(article_status='published')
-        context = {'articles': articles_list}
+        paginator = Paginator(articles_list, 2)
+        page = request.GET.get('page')
+        try:
+            articles = paginator.page(page)
+        except PageNotAnInteger:
+            articles = paginator.page(1)
+        except EmptyPage:
+            articles = paginator.page(paginator.num_pages)
+        context = {'articles': articles,
+                   'page': page}
         return render(request, 'blog/articles/list.html', context)
 
 
